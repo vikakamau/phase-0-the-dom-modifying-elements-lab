dec1c707cdff3a6c97205d8448dc568e76fb4b7c
victor kamau <vikakamau04@gmail.com>
2024-04-02 14:46:58 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+const gk = document.querySelector(`main#main`);
+gk.remove()
+const newHeader = document.createElement("h1")
+newHeader.id = "victory"
+newHeader.textContent = "kamau is the champion"
